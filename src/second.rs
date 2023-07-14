489615b5f0f13e623a92b5972dc89f76ae1676b7
david-moravec <64803040+david-moravec@users.noreply.github.com>
2023-07-14 11:35:12 +0000

    Finish an Ok stack

--- src/second.rs
+++ src/second.rs
@@ -50,6 +50,43 @@ impl<T> Drop for List<T> {
   }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+    pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+        Iter { next: self.head.as_deref() }
+    }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+  
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.elem
+        })
+    }
+}
+
 
 
 #[cfg(test)]
@@ -77,5 +114,28 @@ mod test {
 
     assert_eq!(list.pop(), Some(1));
     assert_eq!(list.pop(), None);
+  }
+  
+  #[test]
+  fn into_iter() {
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
+  
+      let mut iter = list.into_iter();
+      assert_eq!(iter.next(), Some(3));
+      assert_eq!(iter.next(), Some(2));
+      assert_eq!(iter.next(), Some(1));
+      assert_eq!(iter.next(), None);
+  }
+  
+  #[test]
+  fn iter() {
+      let mut list = List::new();
+      list.push(1); list.push(2); list.push(3);
+  
+      let mut iter = list.iter();
+      assert_eq!(iter.next(), Some(&3));
+      assert_eq!(iter.next(), Some(&2));
+      assert_eq!(iter.next(), Some(&1));
   }
 }
